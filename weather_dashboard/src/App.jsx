beb9394dbc92de4944bb9e1dd45cf129a86532d7
kgatatsoo <vibrationbanda@gmail.com>
2025-03-27 09:02:47 +0200

    Updated event listener

--- weather_dashboard/src/App.jsx
+++ weather_dashboard/src/App.jsx
@@ -1,25 +1,39 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 
 function App() {
-  const [data,setData] =useState({})
-  const [location, setLocation] = useState('')
+  const [data, setData] =useState({});
+  const [location, setLocation] = useState('');
 
-  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e5e7b343bdafbd282bc4eb37a3ba050e`
+  const url =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=e5e7b343bdafbd282bc4eb37a3ba050e`;
 
   const searchLocation = (event) => {
-    if (event.key ==='Enter'){
+    if (event.key ==='Enter'&&location){
       axios.get(url).then((response) =>{
         setData(response.data)
         console.log(response.data)
   })
+  .catch((error) =>{
+    console.error("Error retrieving data:",error);
+  });
 }
       
-}
+};
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+        type= "text"
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyDown={searchLocation}
+        placeholder='Enter Location'
+        />
+      </div>
+
+
       <div className="container">
         <div className="top">
           <div className="location">
